1c0def0160fdf9a27ba34c9dc6bac301167b17e1
unknown <fayimarahuman2002@gmail.com>
2024-11-12 11:00:21 +0300

    assignment

--- ObjectOrientedprogramming.py
+++ ObjectOrientedprogramming.py
@@ -11,49 +11,6 @@
 # description
 # start-date
 # end-date
-
-
-
-#Assignment
-#1.create a class with the following
-#class  Cohort:these are the things you need from them
-# Name
-# description
-# start-date
-# end-date
-#add a constructor for cohort class
-#solutions
-class Corhort:
-    name='Fayima Rahuman'
-    description='cohort4student witu'
-    start_date='20/August/2024'
-    end_date='10/december/2024'
-Corhort4=Corhort ()
-print(Corhort4.name) 
-print(Corhort4.description) 
-print(Corhort4.start_date)
-print(Corhort4.end_date)
-   
-
-#2.create a function or method to the class that prints  the cohort name and the total number of students
-class corhort:
-    def __init__(students,name,total_number_of_students):
-        students.name=name
-        students.total_number_of_students=total_number_of_students
-    def myfunc(students):
-            print(f"Corhort name is  {students.name} and the total number of students is  {students.total_number_of_students}")
-Corhort4=corhort('Cohort4',59)   
-Corhort4.myfunc()
-        
-#3.create a new object of the cohort class
-students=Corhort()
-name='fayima'
-age=22
-registration_number='2024/DSC/0034/SS'
-print(students.name)
-
-
-
 #read about the constructors and instances
 # class Person:
 #  def __init__(self, name, age):
@@ -69,3 +26,52 @@ print(students.name)
 #   x = 5
 # p1 = MyClass()
 # print(p1.x)
+
+
+
+
+#Assignment
+#1.create a class with the following
+#class  Cohort:these are the things you need from them
+# Name
+# description
+# start-date
+# end-date
+#add a constructor for cohort class
+#solutions
+class corhort:
+   students_name='Fayima Rahuman'
+   description='Corhort4 student witu'
+   start_date='20/august/2024'
+   end_date='20/December/2024'
+Corhot4=corhort()   
+print(Corhot4.students_name) 
+print(Corhot4.description) 
+print(Corhot4.start_date)
+print(Corhot4.end_date)
+   
+
+#2.create a function or method to the class that prints  the cohort name and the total number of students
+class corhort:
+    def __init__(corhot_details,name,total_number_of_students):
+        corhot_details.name=name
+        corhot_details.total_number_of_students=total_number_of_students
+    def myfunc(cohort_details):
+            print(f"Corhort name is    {cohort_details.name}   and the total number of students is    {cohort_details.total_number_of_students}")
+Corhort4=corhort('Cohort4',59)   
+Corhort4.myfunc()
+        
+#3.create a new object of the cohort class
+class corhot:
+    def __init__(student_details,name,age,):
+        student_details.name=name
+        student_details.age=age
+    def __str__(student_details):
+            return (f"The student name is{student_details.name} and the student age is {student_details.age}")
+students=corhot('Fayima Rahuman','22')
+print(students)           
+    
+
+
+
+#read about the constructors and instances
